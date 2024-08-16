70a56ff65c408ccbc93a78ac4c839794e9b26257
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-08-16 09:57:47 -0700

    v73.0.0

--- src/Recipe/Crm.php
+++ src/Recipe/Crm.php
@@ -11,4 +11,142 @@ namespace Playground\Make\Model\Recipe;
  */
 class Crm extends Playground
 {
+    /**
+     * @var array<string, array<string, mixed>>
+     */
+    protected array $dates = [
+        'canceled_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'closed_at' => [
+            'nullable' => true,
+            'index' => true,
+        ],
+        'embargo_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'fixed_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'planned_end_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'planned_start_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'postponed_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'published_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'released_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'resumed_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'resolved_at' => [
+            'nullable' => true,
+            'index' => true,
+        ],
+        'suspended_at' => [
+            'nullable' => true,
+            'index' => false,
+        ],
+        'timer_end_at' => [
+            'nullable' => true,
+            'index' => true,
+        ],
+        'timer_start_at' => [
+            'nullable' => true,
+            'index' => true,
+        ],
+    ];
+
+    /**
+     * @var array<string, array<string, mixed>>
+     */
+    protected array $factoryStates = [
+        'locked' => [
+            'type' => 'flag',
+            // 'flag' => 'locked',
+            'value' => true,
+        ],
+        'featured' => [
+            'type' => 'flag',
+            'value' => true,
+        ],
+    ];
+
+    /**
+     * @var array<string, array<string, mixed>>
+     */
+    protected array $json = [
+        'address' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+        'assets' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+        'contact' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+        'meta' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+        'notes' => [
+            'default' => '[]',
+            'readOnly' => true,
+            'nullable' => true,
+            'type' => 'JSON_ARRAY',
+            'comment' => 'Array of note objects',
+        ],
+        'options' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+        'sources' => [
+            'default' => '{}',
+            'nullable' => true,
+            'type' => 'JSON_OBJECT',
+        ],
+    ];
+
+    public function init(): void
+    {
+        $this->flags['featured'] = [
+            'type' => 'boolean',
+            'default' => false,
+            'icon' => 'fa-solid fa-star',
+        ];
+        $this->flags['sms'] = [
+            'type' => 'boolean',
+            'default' => false,
+            'icon' => 'fa-solid fa-comment-sms',
+        ];
+
+        $this->columns['phone'] = [
+            'type' => 'string',
+            'nullable' => true,
+        ];
+    }
 }
